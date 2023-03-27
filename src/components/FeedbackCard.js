89659f3eee42ceff5168b784e3538d6ab51a1e62
Oluwaseyi3 <iamoluwaseyiolasupo.com>
2023-03-27 21:02:36 +0100

    removed some refs

--- src/components/FeedbackCard.js
+++ src/components/FeedbackCard.js
@@ -5,14 +5,14 @@ import {gsap, Elastic, Expo} from "gsap"
 
 const FeedbackCard = ({content, name, title , img}) => {
 
-  const cardsRef = useRef()
-  const cards2Ref = useRef()
-  const cards3Ref = useRef()
+  const cardsRef1 = useRef()
+  const cards2Ref2 = useRef()
+  const cards3Ref3 = useRef()
  
   useLayoutEffect(() => {
-    const el = cardsRef.current;
-    const el2 = cards2Ref.current;
-    const el3 = cards3Ref.current;
+    const el = cardsRef1.current;
+    const el2 = cards2Ref2.current;
+    const el3 = cards3Ref3.current;
 
     gsap.from(
       el,
@@ -54,11 +54,11 @@ const FeedbackCard = ({content, name, title , img}) => {
     <div className='flex  bg-red-50 justify-between flex-col rounded border-1   px-10 py-12 rounded-[20px] max-w-[370px] min-h-[300px] md: mr-10 sm:mr-5
     mr-0 my-5 
     '>
-      <p className='font-poppins font-bold font-normal text-[18px] leading-[32px] text-black my-10 ' ref={cardsRef}>
+      <p className='font-poppins font-bold font-normal text-[18px] leading-[32px] text-black my-10 ' ref={cardsRef1}>
         {content}
       </p>
-      <p className='font-poppins  font-normal text-[18px] leading-[32px] text-black' ref={cards2Ref}>{name}</p>
-      <div className='flex flex-row mt-5' ref={cards3Ref}>
+      <p className='font-poppins  font-normal text-[18px] leading-[32px] text-black' ref={cards2Ref2}>{name}</p>
+      <div className='flex flex-row mt-5' ref={cards3Ref3}>
           {/* <img  src={img} alt={name} className="w-[48px] h-[48px] rounded-full"/> */}
           <svg width="33" height="33" viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="16.4438" cy="16.7622" r="16.0254" fill="#E0EFFF"></circle><path d="M18.8477 11.1533L17.7179 12.2831L21.3877 15.9609L8.43118 15.9609L8.43118 17.5635L21.3877 17.5635L17.7099 21.2413L18.8477 22.3711L24.4565 16.7622L18.8477 11.1533Z" fill="#003399"></path></svg>
               

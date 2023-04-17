ee378749089b81de78bc3ea3694dc905712accdb
Oluwaseyi3 <iamoluwaseyiolasupo@gmail.com>
2023-04-17 20:55:13 +0100

    added mores  ui

--- src/components/Background.js
+++ src/components/Background.js
@@ -5,9 +5,9 @@ import Outline from './Outline'
 
 const Background = () => {
   return (
-    <div className='relative text-white h-[70vh] w-[100%] '>
+    <div className='relative text-white max-h-[400px]  w-[100%] '>
      {/* <img src={mask} alt="" className='max-h-[1500px] w-[100%] h-[70%] '/> */}
-     <div className='h-[70%] w-[100%] md:max-h-[400px]  md: w-[100%] bg-blue-600 z-10  rounded-xl top-10 md:w-[100%] md:left-10 flex flex-col gap-5 p-14' >
+     <div className='w-[100%] max-h-[400px] md: w-[100%] bg-blue-600 z-10  rounded-xl top-10 md:w-[100%] md:left-10 flex flex-col gap-5 p-14' >
         <h1 className='text-[20px]'>Take a Step Now</h1>
         <h1 className='text-[30px]'>Start Saving for a better Tommorow</h1>
         <div className='w-[400px]'>
